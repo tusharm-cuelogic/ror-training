0c3347cbba90e42bbab11778178608f42ba06f8d
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2017-06-15 15:16:32 +0530

    Moved all folders to root directory

--- sample_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '3e557fa368796fd1d8eec0153ebe9811538eb7a8bc711c8adc5099ddbea1d690083495d3e6cccd8120ccd551194410756a1a9cc974e7b5321772392df696f8ff'
